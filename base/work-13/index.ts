91c9e1e857579332f56d17bccfdc15ac5656b348
Sultonbek <kingsulton45@gmail.com>
2023-09-10 12:25:34 +0500

    13-work

---
+++ base/work-13/index.ts
@@ -0,0 +1,29 @@
+// console.log(/[a-zA-Z0-9_$]*/.test('привет'))
+
+// console.log('762120,0,22;763827,0,50;750842,0,36;749909,0,95;755884,0,41;'.split(/,.*?;/))
+
+// console.log([...'{"a": 1, "b": "2"}'.matchAll(/"(?<k>.*?)":\s(?<v>.*?)(?=[,}])/g)])
+
+// const format = (str: string, obj) => {
+//   return str.replaceAll(/\${.*?}/g, (match): string => {
+//     return obj[match.replace(/\${|}/g, "")];
+//   });
+// };
+
+// Hello, Bob! Your age is 10.
+// const res = format("Hello, ${user}! Your age is ${age}.", {
+//   user: "Bob",
+//   age: 10,
+// });
+
+// calc(`Какой-то текст (10 + 15 - 24) ** 2 Еще какой то текст 2 * 10`) == `Какой-то текст 1 Еще какой-то текст 20`
+
+const calc = (str) => {
+  return str.replace(/[(]*\d+[+\-*/()\s1-9]+\d+[)]*/g, (arg) => {
+    return eval(arg)
+  });
+};
+
+console.log(
+  calc(`Какой-то текст (10 + 15 - 24) ** 2 Еще какой то текст 2 * 10`)
+);
