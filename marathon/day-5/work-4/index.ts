63a7d48b5a4e274c9b6a9d65d3b3dc27215acafa
Sultonbek <kingsulton45@gmail.com>
2024-02-06 12:16:08 +0500

    day 5 work 4

--- marathon/day-5/work-4/index.ts
+++ marathon/day-5/work-4/index.ts
@@ -1,11 +1,20 @@
 // Поиск в массиве строк по заданной подстроке с пропуском символов
 
-// Необходимо написать функцию, которая бы принимала массив строк и строку и возвращала бы новый массив, 
+// Необходимо написать функцию, которая бы принимала массив строк и строку и возвращала бы новый массив,
 // состоящий только из элементов с содержанием заданной подстроки. Алгоритм должен учитывать, что подстрока может быть найдена в строке при помощи пропуска части символов в строке (нечеткий поиск).
 
-console.log(find('kbza', [
-  'kobezzza',
-  'bob',
-  'kibiza',
-  'kobea'
-])); // ['kobezzza', 'kibiza']
+function find(searchedStr: string, strArr: string[]): string[] {
+  return strArr.filter((str) => {
+    const stack = searchedStr.split("");
+    for (const sym of str) {
+      if (sym === stack[0]) {
+        stack.shift();
+      }
+      if (stack.length === 0) {
+        return str;
+      }
+    }
+  });
+}
+
+console.log(find("kbza", ["kobezzza", "bob", "kibiza", "kobea"])); // ['kobezzza', 'kibiza']
