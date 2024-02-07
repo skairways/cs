bc3c29babb0e087fb2e87a0865b53045c3391c5d
Sultonbek <kingsulton45@gmail.com>
2024-02-07 12:07:41 +0500

    day-6 work-3

--- marathon/day-6/work-2/index.ts
+++ marathon/day-6/work-2/index.ts
@@ -2,12 +2,18 @@
 
 // Должна быть поддержка множественных и одноразовых событий и отмены сразу всех событий по имени.
 
+class EventEmitter {
+  once(event, cb) {}
+  emit(event, cb) {}
+  off(event, cb?) {}
+}
+
 const ee = new EventEmitter();
 
-ee.once('foo', console.log); // Сработает только один раз
+ee.once("foo", console.log); // Сработает только один раз
 
-ee.emit('foo', 1);
-ee.emit('foo', 2);
+ee.emit("foo", 1);
+ee.emit("foo", 2);
 
-ee.off('foo', console.log); // Отмена конкретного обработчика события по ссылке
-ee.off('foo');              // Отмена всех обработчиков этого события
+ee.off("foo", console.log); // Отмена конкретного обработчика события по ссылке
+ee.off("foo"); // Отмена всех обработчиков этого события
--- marathon/day-6/work-3/index.ts
+++ marathon/day-6/work-3/index.ts
@@ -2,5 +2,22 @@
 
 // Необходимо написать функцию, которая бы повторяло поведение Array.prototype.flat.
 
-console.log(flat([[1, 2], [[1]], 2]));    // [1, 2, [1], 2]
+const flat = (arr, depth?: number) => {
+  const result = [];
+
+  for (let iter of arr) {
+    if (Array.isArray(iter)) {
+      if (depth > 0) {
+        iter = flat(iter, --depth);
+      }
+      iter.map((el) => result.push(el));
+    } else {
+      result.push(iter);
+    }
+  }
+
+  return result;
+};
+
+console.log(flat([[1, 2], [[1]], 2])); // [1, 2, [1], 2]
 console.log(flat([[1, 2], [[1]], 2], 2)); // [1, 2, 1, 2]1
