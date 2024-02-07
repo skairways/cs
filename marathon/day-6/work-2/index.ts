53d526d426eee01e31fe691713c19b504f555625
Sultonbek <kingsulton45@gmail.com>
2024-02-07 13:36:35 +0500

    day-6 work-2

--- marathon/day-6/work-2/index.ts
+++ marathon/day-6/work-2/index.ts
@@ -3,9 +3,36 @@
 // Должна быть поддержка множественных и одноразовых событий и отмены сразу всех событий по имени.
 
 class EventEmitter {
-  once(event, cb) {}
-  emit(event, cb) {}
-  off(event, cb?) {}
+  eventBus = new Map();
+
+  once(event, cb) {
+    let fns;
+    if (this.eventBus.has(event)) {
+      fns = this.eventBus.get(event);
+    } else {
+      fns = new Set();
+    }
+    fns.add(cb);
+    this.eventBus.set(event, fns);
+  }
+
+  emit(event, ...args) {
+    if (this.eventBus.has(event)) {
+      const fns = this.eventBus.get(event);
+      fns.forEach((fn) => fn.apply(this, args));
+    }
+  }
+
+  off(event, cb?) {
+    if (cb === undefined) {
+      this.eventBus.clear();
+    }
+
+    if (this.eventBus.has(event) && cb) {
+      const fns: Set<Function> = this.eventBus.get(event);
+      fns.delete(cb);
+    }
+  }
 }
 
 const ee = new EventEmitter();
@@ -16,4 +43,5 @@ ee.emit("foo", 1);
 ee.emit("foo", 2);
 
 ee.off("foo", console.log); // Отмена конкретного обработчика события по ссылке
+
 ee.off("foo"); // Отмена всех обработчиков этого события
